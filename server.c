5a52f9dc7c97a44ba7dfe8dae26735101d83e462
James Booth <boothj5@gmail.com>
2014-08-08 01:11:22 +0100

    Added error handling

--- server.c
+++ server.c
@@ -5,48 +5,54 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
 
-typedef struct thread_data_t {
+typedef struct thread_client_t {
     char *ip;
     int port;
     int sock;
 } ThreadData;
 
-void *connection_handler(void *data)
+void* connection_handler(void *data)
 {
     ThreadData *client = (ThreadData *)data;
     int read_size;
-     
-    printf("Connection handler assigned for ip: %s, port: %d.\n", client->ip, client->port);
+    char *client_message;
 
-    char *client_message = malloc(sizeof(char) * 2000);
-    while((read_size = recv(client->sock, client_message, 2000 , 0)) > 0 ) {
-        int act_size = strlen(client_message);
-        printf("Received message from: %s:%d: \"%s\" (read_size %d, strlen %d).\n",
-            client->ip, client->port, client_message, read_size, act_size);
-        write(client->sock, client_message, read_size);
-        free(client_message);
+    while(1) {
         client_message = malloc(sizeof(char) * 2000);
-    }
-     
-    if(read_size == 0) {
-        printf("Client disconnected, ip: %s, port %d.\n", client->ip, client->port);
-        fflush(stdout);
+
+        errno = 0;
+        read_size = recv(client->sock, client_message, 2000 , 0);
+
+        // error
+        if (read_size == -1) {
+            perror("Error receiving on connection");
+            free(client);
+            free(client_message);
+            break;
+
+        // client closed
+        } else if (read_size == 0) {
+            printf("%s:%d - Client disconnected.\n", client->ip, client->port);
+            free(client);
+            free(client_message);
+            break;
+
+        // successful recv
+        } else {
+            write(client->sock, client_message, read_size);
+            free(client_message);
+        }
     }
 
-    else if(read_size == -1) {
-        perror("recv failed.");
-    }
-
-    free(client);
-         
     return 0;
 }
 
 int main(int argc , char *argv[])
 {
-    int socket_desc, new_socket, c;
-    struct sockaddr_in server, client;
+    int socket_desc, client_socket, c, ret;
+    struct sockaddr_in server_addr, client_addr;
 
     if (argc != 2) {
         printf("Please enter a port.\n");
@@ -55,40 +61,50 @@ int main(int argc , char *argv[])
 
     int port = atoi(argv[1]);
      
+    errno = 0;
     socket_desc = socket(AF_INET, SOCK_STREAM, IPPROTO_IP); // ipv4, tcp, ip
     if (socket_desc == -1) {
-        printf("Could not create socket.");
+        perror("Could not create socket");
     }
      
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
      
-    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        puts("Bind failed.");
+    errno = 0;
+    ret = bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (ret == -1) {
+        perror("Bind failed");
     }
-    puts("Bind successfull.");
-     
-    listen(socket_desc, 20);
-     
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
 
-    while ((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
-        ThreadData *data = malloc(sizeof(ThreadData));
-        data->ip = inet_ntoa(client.sin_addr);
-        data->port = ntohs(client.sin_port);
-        data->sock = new_socket;
+    errno = 0;
+    ret = listen(socket_desc, 20);
+    if (ret == -1) {
+        perror("Listen failed");
+    }
+    puts("Waiting for incoming connections...");
+
+    while (1) {
+        c = sizeof(struct sockaddr_in);
+
+        errno = 0;
+        client_socket = accept(socket_desc, (struct sockaddr *)&client_addr, (socklen_t*)&c);
+        if (client_socket == -1) {
+            perror("Accept failed");
+        }
+
+        ThreadData *client = malloc(sizeof(ThreadData));
+        client->ip = inet_ntoa(client_addr.sin_addr);
+        client->port = ntohs(client_addr.sin_port);
+        client->sock = client_socket;
 
         pthread_t sniffer_thread;
-        if (pthread_create(&sniffer_thread, NULL, connection_handler, (void *)data) < 0) {
-            perror("Could not create thread.");
-            return 1;
+        ret = pthread_create(&sniffer_thread, NULL, connection_handler, (void *)client);
+        if (ret == 0) {
+            printf("%s:%d - Connection handler assigned.\n", client->ip, client->port);
+        } else {
+            puts("Could not create thread.");
         }
-    }
-
-    if (new_socket < 0) {
-        perror("accept failed.");
     }
  
     return 0;
